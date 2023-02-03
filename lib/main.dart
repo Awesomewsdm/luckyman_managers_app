4d65ab2ce5f4ce8d76efdb1beac02b715228da5d
Awesomewsdm <88857387+Awesomewsdm@users.noreply.github.com>
2023-02-03 18:36:33 +0000

    home screen created

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:luckyman_managers_app/Screens/home_screen.dart';
 import 'firebase_options.dart';
 
 // ...
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const GetMaterialApp(
-
+      home: HomeScreen(),
     );
   }
 }
